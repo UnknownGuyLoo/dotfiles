20b3ab822e1b1d290841d4d1723f9d9f10aabfb1
UnknownGuyLoo <mujtabashaikh2007@gmail.com>
2024-12-30 18:37:02 +0530

    script for changing wallpaper, added keybind for it as well

---
+++ setwall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+effects=("grow" "wave" "any" "fade")
+random_index=$(( RANDOM % ${#effects[@]} )) 
+img=$(sxiv -to ~/.dotfiles/wallpapers/ | awk -F'/' '{print $NF}')
+swww img -t ${effects[random_index]} ~/.dotfiles/wallpapers/$img
